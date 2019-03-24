17423e8af7a4542c084a571f44bb35faf5025f34
sstanytska <stanytskasofia@gmail.com>
2019-03-24 16:14:44 -0500

    changes properties

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node {
-    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please provide IP host', name: 'IP', trim: true), string(defaultValue: 'latest', description: 'What version would you like to apply?', name: 'VER', trim: true)])])
+    properties([parameters([string(defaultValue: '157.230.176.35', description: 'Default value', name: 'IP', trim: true), string(defaultValue: 'latest', description: 'What version would you like to apply?', name: 'VER', trim: true), string(defaultValue: '4000', description: 'What port would you like to run', name: 'PORT', trim: true)])])
     stage("Remove conteiner"){
         try{
             sh "ssh root@${IP} docker rm -f flaksex"
